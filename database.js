eb1be625d14c1f155b46cfd5d4a7d6a8f5c6c77e
emmeravivar <em.meravivar@gmail.com>
2023-08-24 14:45:13 +0200

    update old code

--- database.js
+++ database.js
@@ -3,13 +3,14 @@
 	// Your web app's Firebase configuration
 	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 	const firebaseConfig = {
-		apiKey: "AIzaSyB50XToHhvYNG2LSQ1dsOC4FPXEUWRwV6E",
-		authDomain: "courso-ee0b2.firebaseapp.com",
-		databaseURL: "https://courso-ee0b2-default-rtdb.europe-west1.firebasedatabase.app",
-		projectId: "courso-ee0b2",
-		storageBucket: "courso-ee0b2.appspot.com",
-		messagingSenderId: "853168541102",
-		appId: "1:853168541102:web:3bf514e1378b4510a78c3f"
+		apiKey: "AIzaSyBboZP3WoD5Os9mOv4QGWej9Cu5lmI-bIM",
+		authDomain: "course-92aae.firebaseapp.com",
+		databaseURL: "https://course-92aae.firebaseio.com",
+		projectId: "course-92aae",
+		storageBucket: "course-92aae.appspot.com",
+		messagingSenderId: "906967504618",
+		appId: "1:906967504618:web:a5b51551a229502dc7a993",
+		measurementId: "G-PWBBC8GN6B"
 	};
 	// Initialize Firebase
 	firebase.initializeApp(firebaseConfig);
